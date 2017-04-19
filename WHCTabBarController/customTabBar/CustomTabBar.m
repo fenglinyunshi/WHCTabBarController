64f5d2aa64280f4f8007fde62ec0e95f62db43c8
Dustin <fenglinyunshi@126.com>
2017-04-19 16:31:55 +0800

    解决了中间按钮点击上半部分不响应的bug。

--- WHCTabBarController/customTabBar/CustomTabBar.m
+++ WHCTabBarController/customTabBar/CustomTabBar.m
@@ -93,6 +93,21 @@
         [_delegate myTabbar:self btnClicked:btn.tag];
     }
 }
+#pragma mark 当按钮超过了父视图范围，点击是没有反应的。因为消息的传递是从最下层的父视图开始调用hittest方法。当存在view时才会传递对应的event，现在点击了父视图以外的范围，自然返回的是nil。所以当子视图（比如按钮btn）因为一些原因超出了父视图范围，就要重写hittest方法，让其返回对应的子视图，来接收事件。
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event{
+    UIView *view = [super hitTest:point withEvent:event];
+    if (view == nil) {
+        
+        UIButton *btn = (UIButton *)[self viewWithTag:2];
+        CGPoint tempoint = [btn convertPoint:point fromView:self];
+        //判断给定的点是否被一个CGRect包含,可以用CGRectContainsPoint函数
+        if (CGRectContainsPoint(btn.bounds, tempoint))
+        {
+            view = btn;
+        }
+    }
+    return view;
+}
 
 
 @end
